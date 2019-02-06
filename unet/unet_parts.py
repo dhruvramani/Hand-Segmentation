90fa646c4f8f050f2aa3bd5b17a9b1a816af500a
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-07 01:36:43 +0530

    lil debug

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -68,7 +68,16 @@ class up(nn.Module):
         x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
                         diffY // 2, int(diffY / 2)))
 
-        x = torch.cat([x2[:, :, :-1, :], x1[:, :, :, :-1]], dim=1)
+        if(diffX > 0):
+            x1 = x1[:, :, :-diffX, :]
+        else :
+            x2 = [:, :, :diffX, :]
+
+        if(diffY > 0):
+            x1 = x1[:, :, :, :-diffY]
+        else :
+            x2 = [:, :, :, :diffY]
+        x = torch.cat([x2, x1], dim=1)
         x = self.conv(x)
         return x
 
