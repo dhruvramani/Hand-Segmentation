5bd976f2dd92a942895e9ae2703786413dd8c8eb
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-07 01:30:27 +0530

    lil debug

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -69,7 +69,7 @@ class up(nn.Module):
                         diffY // 2, int(diffY / 2)))
         if(x1.shape[2] != x2.shape[2]):
             x = torch.cat([x2[:, :, :-1, :], x1], dim=1)
-        else 
+        else :
             x = torch.cat([x2, x1], dim=1)
         x = self.conv(x)
         return x
