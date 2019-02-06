31fef3b500e136c709d4d311e13e7cee72eef6e7
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-06 11:43:06 +0530

    erode

--- erode.py
+++ erode.py
@@ -10,10 +10,15 @@ args = parser.parse_args()
 
 img1 = cv2.imread(args.test, 3)
 img2 = cv2.imread(args.mask, 0) 
-kernel = np.ones((2,2), np.uint8) 
+seg_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+_, fg_mask = cv2.threshold(seg_gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+fg_mask = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR)
+fg = cv2.bitwise_and(img, fg_mask)
+
+'''kernel = np.ones((2,2), np.uint8) 
 img2 = cv2.erode(img2, kernel, iterations=2)
 
 width, height, depth = img1.shape
-combinedImage = cv2.merge((img1, img2))
+combinedImage = cv2.merge((img1, img2))'''
 
-cv2.imwrite(args.save, combinedImage)
+cv2.imwrite(args.save, fg)
