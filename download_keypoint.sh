51e41fda87f793a9e394eca82110d80d76bffb30
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-07 00:54:07 +0530

    sh change

--- download_keypoint.sh
+++ download_keypoint.sh
@@ -1,10 +1,10 @@
 
 # ------------------------- BODY, FACE AND HAND MODELS -------------------------
 # Downloading body pose (COCO and MPI), face and hand models
-OPENPOSE_URL="http://posefs1.perception.cs.cmu.edu/OpenPose/models/"
-HAND_FOLDER="./utils/"
+OPENPOSE_URL="http://posefs1.perception.cs.cmu.edu/OpenPose/models/hand/"
+HAND_FOLDER="./utils/pose_iter_102000.caffemodel"
 
 # "------------------------- HAND MODELS -------------------------"
 # Hand
-HAND_MODEL=$HAND_FOLDER"pose_iter_102000.caffemodel"
-wget -c ${OPENPOSE_URL}${HAND_MODEL} -P ${HAND_FOLDER}
+HAND_MODEL=$OPENPOSE_URL"pose_iter_102000.caffemodel"
+wget -c ${HAND_MODEL} -P ${HAND_FOLDER}
