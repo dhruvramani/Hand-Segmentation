3af07425addd101d47245a94ad6a046cdbede73d
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-11 21:50:19 +0530

    debug

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -48,8 +48,8 @@ def mark_keypoints(path, destination, dist=True):
                 phi = math.pi + theta
                 p3, p4 = list(p1), list(p2)
                 while(list(frame[p3[0], p3[1]]) != [0, 0, 0]):
-                    p3[0] = int(p1[0] + 1 * math.cos(theta))
-                    p3[1] = int(p1[1] + 1 * math.cos(theta))
+                    p3[0] = int(p1[0] + 2 * math.cos(theta))
+                    p3[1] = int(p1[1] + 2 * math.cos(theta))
 
                 while(list(frame[p4[0], p4[1]]) != [0, 0, 0]):
                     p4[0] = int(p1[0] + 1 * math.cos(phi))
