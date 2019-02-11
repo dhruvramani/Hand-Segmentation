c855ca4da764983abc070208f0c8bf07f385c837
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-11 21:29:12 +0530

    debug

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -42,10 +42,10 @@ def mark_keypoints(path, destination, dist=True):
         for pair in POSE_PAIRS:
             p1, p2 = points[pair[0]], points[pair[1]]
             if p1 and p2:
+                print(p1, p2)
                 theta = (math.pi / 2) - math.atan((p2[1] - p1[1]) / (p2[0] - p1[0]))
                 phi = math.pi + theta
                 p3, p4 = list(p1), list(p1)
-                #print(list(frame[p3[0], p3[1]]) != [0, 0, 0])
                 while(list(frame[p3[0], p3[1]]) != [0, 0, 0]):
                     p3[0] = int(p1[0] + 1 * math.cos(theta))
                     p3[1] = int(p1[1] + 1 * math.cos(theta))
