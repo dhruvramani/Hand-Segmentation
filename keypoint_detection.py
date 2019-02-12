ee1cfbeaca09a4e7accce7db6789bd08e96f40d9
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-12 18:12:28 +0530

    image 1

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -56,12 +56,12 @@ def mark_keypoints(path, destination, dist=True):
                     while(net_black(frame, p3) != 0):
                         p3[0] = math.ceil(p1[0] + coo * math.cos(theta))
                         p3[1] = math.ceil(p1[1] + coo * math.sin(theta))
-                        coo += 1
+                        coo += 2
                     coo = 0
                     while(net_black(frame, p4) != 0):
                         p4[0] = math.ceil(p1[0] - coo * math.cos(theta))
                         p4[1] = math.ceil(p1[1] - coo * math.sin(theta))
-                        coo += 1
+                        coo += 2
                 except :
                     print("Ignored")
                     continue
