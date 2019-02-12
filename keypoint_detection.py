806e8112b6e764f11853a45b089e6c7f6fa11988
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-12 18:15:34 +0530

    image 1

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -16,8 +16,8 @@ POSE_PAIRS = [  [0,1], [1,2], [2,3],
 npoints = 22
 net = cv2.dnn.readNetFromCaffe(_PROTPATH, _WEIGHTPATH)
 
-def net_black(frame, coord, size=300):
-    return int(np.mean(frame[coord[0] - size : coord[0] + size, coord[1] - size : coord[1] + size, :]))
+def net_black(frame, coord, size=50):
+    return int(np.sum(frame[coord[0] - size : coord[0] + size, coord[1] - size : coord[1] + size, :]))
 
 def mark_keypoints(path, destination, dist=True):
     frame = cv2.imread(path)
