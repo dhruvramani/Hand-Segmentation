83e97da743411fd59b004f42e3bed75f6bd19499
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-12 17:44:50 +0530

    image 1

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -50,12 +50,12 @@ def mark_keypoints(path, destination, dist=True):
                 p3, p4 = list(p1), list(p1)
                 try :
                     while(list(frame[p3[0], p3[1]]) != [0, 0, 0]):
-                        p3[0] = math.ceil(p1[0] + 1.5 * math.cos(theta))
-                        p3[1] = math.ceil(p1[1] + 1.5 * math.sin(theta))
+                        p3[0] = math.ceil(p1[0] + 5 * math.cos(theta))
+                        p3[1] = math.ceil(p1[1] + 5 * math.sin(theta))
 
                     while(list(frame[p4[0], p4[1]]) != [0, 0, 0]):
-                        p4[0] = math.ceil(p4[0] - 1.5 * math.cos(theta))
-                        p4[1] = math.ceil(p4[1] - 1.5 * math.sin(theta))
+                        p4[0] = math.ceil(p4[0] - 5 * math.cos(theta))
+                        p4[1] = math.ceil(p4[1] - 5 * math.sin(theta))
                 except IndexError:
                     print("Ignored")
                     continue
