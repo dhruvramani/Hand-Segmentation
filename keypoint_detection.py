87d259a5659a60a2a43f7cf34356ebff50147056
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-12 17:35:08 +0530

    image 1

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -44,7 +44,7 @@ def mark_keypoints(path, destination, dist=True):
             if p1 and p2 and p2[0] != p1[0]:
                 print(pair)
                 theta =  (math.pi / 2) + math.atan((p2[1] - p1[1]) / (p2[0] - p1[0])) 
-                phi = (math.pi) + math.atan((p2[1] - p1[1]) / (p2[0] - p1[0])) 
+                phi = - (math.pi / 2) + math.atan((p2[1] - p1[1]) / (p2[0] - p1[0])) 
                 p3, p4 = list(p1), list(p2)
                 #while(list(frame[p3[0], p3[1]]) != [0, 0, 0]):
                 p3[0] = math.ceil(p1[0] + 100 * math.cos(theta))
@@ -56,7 +56,7 @@ def mark_keypoints(path, destination, dist=True):
 
                 #cv2.line(frame, (p1[0], p1[1]), (p2[0], p2[1]), (0, 255, 0), 2)
                 cv2.line(frame, (p1[0], p1[1]), (p3[0], p3[1]), (0, 255, 0), 2)
-                #cv2.line(frame, (p1[0], p1[1]), (p4[0], p4[1]), (0, 255, 0), 2)
+                cv2.line(frame, (p1[0], p1[1]), (p4[0], p4[1]), (0, 255, 0), 2)
                 dist = "{0:0.1f}".format(math.sqrt((p4[1] - p3[1])**2 + (p4[0] - p3[0])**2))
                 #cv2.putText(frame, "{}".format(dist), (int(p1[0]), int(p1[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, lineType=cv2.LINE_AA)
 
