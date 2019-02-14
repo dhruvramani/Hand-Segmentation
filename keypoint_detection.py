541f0d13dba16e9f3b4e023af7d41102c9d5b8b3
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-14 22:33:19 +0530

    debug

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -59,17 +59,17 @@ def mark_keypoints(path, destination, out_path, dist=True):
                     p3[0] = int(p1[0] + coo * math.cos(theta))
                     p3[1] = int(p1[1] + coo * math.sin(theta))
                     coo += 1
-                coo = 0
-                while(list(outframe[p4[0], p4[1]]) != [0, 0, 0]):
-                    p4[0] = math.ceil(p1[0] - coo * math.cos(theta))
-                    p4[1] = math.ceil(p1[1] - coo * math.sin(theta))
-                    coo += 1
+                #coo = 0
+                #while(list(outframe[p4[0], p4[1]]) != [0, 0, 0]):
+                #    p4[0] = math.ceil(p1[0] - coo * math.cos(theta))
+                #    p4[1] = math.ceil(p1[1] - coo * math.sin(theta))
+                #    coo += 1
                 #except :
                 #    print("Ignored")
                 #    continue
                 #cv2.line(frame, (p1[0], p1[1]), (p2[0], p2[1]), (0, 255, 0), 2)
                 cv2.line(frame, (p1[0], p1[1]), (p3[0], p3[1]), (0, 255, 0), 2)
-                cv2.line(frame, (p1[0], p1[1]), (p4[0], p4[1]), (0, 255, 0), 2)
+                #cv2.line(frame, (p1[0], p1[1]), (p4[0], p4[1]), (0, 255, 0), 2)
                 dist = "{0:0.1f}".format(math.sqrt((p4[1] - p3[1])**2 + (p4[0] - p3[0])**2))
                 #cv2.putText(frame, "{}".format(dist), (int(p1[0]), int(p1[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, lineType=cv2.LINE_AA)
 
