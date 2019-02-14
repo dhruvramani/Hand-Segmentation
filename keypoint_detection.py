a8a9c77518629ca142e15593b813efab77a2b2de
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-14 22:16:27 +0530

    debug

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -54,7 +54,7 @@ def mark_keypoints(path, destination, out_path, dist=True):
                 p3, p4 = list(p1), list(p1)
                 #try :
                 coo = 0
-                while(list(outframe[p3[0], p3[1]]) != [0, 0, 250]):
+                while(list(outframe[p3[0], p3[1]]) != [0, 0, 0]):
                     p3[0] = math.ceil(p1[0] + coo * math.cos(theta))
                     p3[1] = math.ceil(p1[1] + coo * math.sin(theta))
                     coo += 1
