26b0e50233a6d074dd09582e5475101f3f3047af
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-20 16:07:58 +0530

    test

--- keypoint_detection.py
+++ keypoint_detection.py
@@ -61,8 +61,8 @@ def mark_keypoints(path, destination, out_path, dist=True):
                 #print(outframe[p3[0], p3[1]])
 
                 while(list(outframe[p3[0], p3[1]]) != [0, 0, 0] and coo < 50):
-                    p3[0] = p1[0] + coo * math.cos(theta)
-                    p3[1] = p1[1] + coo * math.sin(theta)
+                    p3[0] = int(p1[0] + coo * math.sin(theta))
+                    p3[1] = int(p1[1] + coo * math.cos(theta))
                     coo += 1
                 #coo = 0
                 #while(list(outframe[p4[0], p4[1]]) != [0, 0, 0]):
@@ -72,7 +72,7 @@ def mark_keypoints(path, destination, out_path, dist=True):
                 #except :
                 #    print("Ignored")
                 #    continue
-                cv2.line(outframe, (p1[0], p1[1]), (p2[0], p2[1]), (0, 255, 0), 2)
+                #cv2.line(outframe, (p1[0], p1[1]), (p2[0], p2[1]), (0, 255, 0), 2)
                 cv2.line(outframe, (p1[0], p1[1]), (p3[0], p3[1]), (0, 255, 0), 2)
                 #cv2.line(frame, (p1[0], p1[1]), (p3[0], p3[1]), (0, 255, 0), 2)
                 #cv2.line(frame, (p1[0], p1[1]), (p4[0], p4[1]), (0, 255, 0), 2)
