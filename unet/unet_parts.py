d30a19adb0c513adae1deab86f5310cb79356d39
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-21 09:24:53 +0530

    plis work

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -65,8 +65,10 @@ class up(nn.Module):
         x1 = self.up(x1)
         diffX = x1.size()[2] - x2.size()[2]
         diffY = x1.size()[3] - x2.size()[3]
-        x2 = F.pad(x2, (int(diffX / 2), int(diffX / 2),
-                        int(diffY / 2), int(diffY / 2)))
+        print(diffX, diffY)
+        _ = input("")
+        x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
+                        diffY // 2, int(diffY / 2)))
 
         x = torch.cat([x2, x1], dim=1)
         x = self.conv(x)
