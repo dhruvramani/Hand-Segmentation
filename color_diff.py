7aae56d295567de6ffbd8d341b3df2ed84f27f23
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-21 11:38:22 +0530

    trying

--- color_diff.py
+++ color_diff.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-'''
-lower = np.array([0, 48, 80], dtype = "uint8")
-upper = np.array([20, 255, 255], dtype = "uint8")
+
+lower = np.array([0,133,85], dtype = "uint8")
+upper = np.array([255,170,125], dtype = "uint8")
 
 def seperate(path, destination):
     frame = cv2.imread(path)
@@ -14,20 +14,6 @@ def seperate(path, destination):
     skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
     cv2.imwrite(destination, skinMask)
     skin = cv2.bitwise_and(frame, frame, mask = skinMask)
-'''
-
-bgSubThreshold = 50
-learningRate = 0
-bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
-
-def remove_background(path, destination):
-    frame = cv2.imread(path)
-    fgmask = bgModel.apply(frame, learningRate=learningRate)
-    kernel = np.ones((3, 3), np.uint8)
-    fgmask = cv2.erode(fgmask, kernel, iterations=1)
-    cv2.imwrite(destination, fgmask)
-    res = cv2.bitwise_and(frame, frame, mask=fgmask)
-    return res
 
 if __name__ == '__main__':
-    remove_background("./test1.jpg", "./test1_out.jpg")
+    seperate("./hand.jpg", "./hand_out.jpg")
