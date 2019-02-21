2e4a8b855543473786abe8ef471a230208bc272b
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-21 11:14:06 +0530

    trying

---
+++ color_diff.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+lower = np.array([0, 48, 80], dtype = "uint8")
+upper = np.array([20, 255, 255], dtype = "uint8")
+
+def seperate(path, destination):
+    frame = cv2.imread(path)
+    converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    skinMask = cv2.inRange(converted, lower, upper)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+    skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+    cv2.imwrite(destination, skinMask)
+    skin = cv2.bitwise_and(frame, frame, mask = skinMask)
+
+if __name__ == '__main__':
+    seperate("./hand.jpg", "./hand_out.jpg")
