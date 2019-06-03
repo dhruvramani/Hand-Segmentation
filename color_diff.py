46933cc871bfd9397f069c527353c93d0f516da9
Dhruv <dhruvramani98@gmail.com>
2019-06-03 20:00:43 +0530

    made changes, should work now

--- color_diff.py
+++ color_diff.py
@@ -19,16 +19,15 @@ def seperate(path, destination):
 def contour(path, destination):
     image = cv2.imread(path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 40, 55, apertureSize = 3)
-    #gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    #thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
-    #thresh = cv2.erode(thresh, None, iterations=2)
-    #thresh = cv2.dilate(thresh, None, iterations=2)
-    #cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #cnts = imutils.grab_contours(cnts)
-    #c = max(cnts, key=cv2.contourArea)
-    #cv2.drawContours(image, [c], -1, (0, 255, 255), 2)
-    cv2.imwrite(destination, edges)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.erode(thresh, None, iterations=2)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key=cv2.contourArea)
+    cv2.drawContours(image, [c], -1, (0, 255, 255), 2)
+    cv2.imwrite(destination, image)
 
 
 
