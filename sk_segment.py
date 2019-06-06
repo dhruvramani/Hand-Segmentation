6c2e6b9e68cf76a0874e6a378c3ef602504bfc72
Dhruv <dhruvramani98@gmail.com>
2019-06-06 13:49:39 +0530

    trying

--- sk_segment.py
+++ sk_segment.py
@@ -1,31 +1,24 @@
+import os, sys, glob, time
 import numpy as np
-from scipy import ndimage as ndi
-from skimage.filters import sobel
-from skimage.feature import canny
-from skimage.exposure import histogram
-from skimage.morphology import watershed
-from skimage.io import imread, imsave, imshow
+from PIL import Image
+import matplotlib.pyplot as plt
+from skimage.segmentation import mark_boundaries
+import imsegm.utilities.data_io as tl_data
+import imsegm.pipelines as segm_pipe
+
+sys.path += [os.path.abspath('.'), os.path.abspath('..')] # Add path to root
 
 def segment(filepath, outfilepath):
-    image = imread(filepath, as_gray=True)
-    hist, hist_centers = histogram(image)
-    edges = canny(image/255.)
-    fill_edges = ndi.binary_fill_holes(edges)
-    
-    label_objects, nb_labels = ndi.label(fill_edges)
-    sizes = np.bincount(label_objects.ravel())
-    mask_sizes = sizes > 20
-    mask_sizes[0] = 0
-    hand_cleaned = mask_sizes[label_objects]
-
-    markers = np.zeros_like(image)
-    markers[image < 30] = 0
-    markers[image > 100] = 1
-
-    elevation_map = sobel(image)
-    segmentation = watershed(elevation_map, markers)
-    segmentation = ndi.binary_fill_holes(segmentation - 1)
-    imsave(outfilepath, segmentation)
+    img = np.array(Image.open(filepath))
+    nb_classes = 2
+    sp_size = 25
+    sp_regul = 0.2
+    dict_features = {'color': ['mean', 'std', 'median']}
+    model, _ = segm_pipe.estim_model_classes_group([img], nb_classes, sp_size=sp_size, sp_regul=sp_regul, dict_features=dict_features, pca_coef=None, model_type='GMM')
+    dict_debug = {}
+    seg, _ = segm_pipe.segment_color2d_slic_features_model_graphcut(img, model, sp_size=sp_size, sp_regul=sp_regul, dict_features=dict_features, gc_regul=5., gc_edge_type='color', debug_visual=dict_debug)
+    plt.imshow(seg)
+    plt.savefig(outfilepath)
 
 if __name__ == '__main__':
     segment("./test_images/hand2.jpg", "./hand2_erod.jpg")
